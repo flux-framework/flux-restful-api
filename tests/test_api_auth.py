4c46c637219d83c016ac3defbae89e2f9b1ca335
vsoch <vsoch@users.noreply.github.com>
2022-11-04 20:48:07 -0600

    lint
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- tests/test_api_auth.py
+++ tests/test_api_auth.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
 import os
 import sys
+
+from fastapi.testclient import TestClient
 
 here = os.path.abspath(os.path.dirname(__file__))
 root = os.path.dirname(here)
@@ -10,25 +10,27 @@ examples = os.path.join(root, "examples")
 sys.path.insert(0, examples)
 sys.path.insert(0, root)
 
-from flux_restful_client import get_basic_auth # noqa
+from flux_restful_client import get_basic_auth  # noqa
 
 # Define authentication in environment for server
-flux_user = 'fluxuser'
+flux_user = "fluxuser"
 flux_token = "12345"
-os.environ['FLUX_USER'] = flux_user
-os.environ['FLUX_TOKEN'] = flux_token
-os.environ['FLUX_REQUIRE_AUTH'] = "true"
+os.environ["FLUX_USER"] = flux_user
+os.environ["FLUX_TOKEN"] = flux_token
+os.environ["FLUX_REQUIRE_AUTH"] = "true"
 
 
-from app.main import app
+from app.main import app  # noqa
 
 client = TestClient(app)
+
 
 def get_headers():
     auth_header = get_basic_auth(flux_user, flux_token)
     if isinstance(auth_header, bytes):
         auth_header = auth_header.decode("utf-8")
-    return {"Authorization" :"Basic %s" % auth_header}
+    return {"Authorization": "Basic %s" % auth_header}
+
 
 def test_submit_list_job():
 
@@ -46,15 +48,15 @@ def test_submit_list_job():
     assert response.status_code == 200
     result = response.json()
     assert "id" in result
-    jobid = result['id']
+    jobid = result["id"]
 
     response = client.get("/jobs", headers=headers)
     assert response.status_code == 200
     result = response.json()
     assert "jobs" in result
-    result = result['jobs']    
+    result = result["jobs"]
     assert len(result) == 1
-    assert result[0]['id'] == jobid
+    assert result[0]["id"] == jobid
 
 
 def test_cancel_job():
@@ -62,10 +64,12 @@ def test_cancel_job():
     headers = get_headers()
 
     # Now submit a job, ensure we get one job back
-    response = client.post("/jobs/submit", json={"command": "sleep 10"}, headers=headers)
+    response = client.post(
+        "/jobs/submit", json={"command": "sleep 10"}, headers=headers
+    )
     assert response.status_code == 200
     result = response.json()
-    jobid = result['id']
+    jobid = result["id"]
     response = client.post(f"/jobs/{jobid}/cancel", headers=headers)
     assert response.status_code == 200
     # TODO we don't have way to actually verify this!
