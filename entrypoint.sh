30ab9727da25e3d5db216be7cf1ca5efff55e4be
vsoch <vsoch@users.noreply.github.com>
2022-11-16 18:34:27 -0700

    try custom entrypoint to update app from branch
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+install_branch=${INSTALL_BRANCH}
+install_repo=${INSTALL_REPO:-flux-framework/flux-restful-api}
+
+# If we are given a custom branch to install, do that first
+if [ ! -z ${install_branch+x} ]; then
+    printf "Custom install of https://github.com:${install_repo}@${install_branch}"
+    rm -rf /code
+    git clone -b ${install_branch} https://github.com:${install_repo} /code
+fi
+
+# Ensure we are still in /code as PWD
+cd /code
 flux start uvicorn app.main:app --host=${HOST} --port=${PORT}
