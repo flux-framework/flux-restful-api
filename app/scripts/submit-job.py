34c3f5a6920d97d1b473e26578533049015157ca
vsoch <vsoch@users.noreply.github.com>
2023-03-06 16:06:52 -0700

    add submit script
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- app/scripts/submit-job.py
+++ app/scripts/submit-job.py
@@ -7,4 +7,5 @@ import flux.job
 
 payload = sys.stdin.read()
 fluxjob = flux.job.JobspecV1.from_yaml_stream(payload)
-print(flux.job.submit_async(flux.Flux(), payload).get_id().decode("utf-8"))
+job = flux.job.submit_async(flux.Flux(), payload)
+print(str(job.get_id()))
